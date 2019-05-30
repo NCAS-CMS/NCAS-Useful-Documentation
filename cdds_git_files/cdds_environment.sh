cf7295dc179afbc9b38666fc3cd2de46edbd59f6
Valeriu Predoi <valeriu.predoi@gmail.com>
2019-05-30 15:15:11 +0100

    changed to new env built with yml env file

--- cdds_git_files/cdds_environment.sh
+++ cdds_git_files/cdds_environment.sh
@@ -1,4 +1,5 @@
+# built using new environment.yml
 export PATH=/home/users/valeriu/miniconda2Dec2018/bin:$PATH
-export PATH=/home/users/valeriu/miniconda2Dec2018/envs/cdds/lib/python2.7/site-packages:$PATH
-export PATH=/home/users/valeriu/miniconda2Dec2018/envs/cdds/bin:$PATH
+export PATH=/home/users/valeriu/miniconda2Dec2018/envs/cdds_new/lib/python2.7/site-packages:$PATH
+export PATH=/home/users/valeriu/miniconda2Dec2018/envs/cdds_new/bin:$PATH
 export PATH=/apps/contrib/metomi/bin:$PATH
